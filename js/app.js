247b76635964c230a8115ffb3d3c976f0d22d0ab
nyarumo <motoo.naruse@gmail.com>
2026-09-14 12:52:33 +0900

    ホーム画面追加用のアイコンと設定を入れる
    スマホのブラウザからホーム画面に置けるようにする。

--- js/app.js
+++ js/app.js
@@ -122,4 +122,8 @@
   }
 
   showToday();
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
 })();
---
+++ sw.js
@@ -0,0 +1,55 @@
+var CACHE = "meigen-shell-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/quotes.js",
+  "./js/today.js",
+  "./js/net.js",
+  "./js/app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return key !== CACHE;
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  var request = event.request;
+  if (request.method !== "GET") return;
+  var url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(request).then(function (cached) {
+      if (cached) return cached;
+      return fetch(request).then(function (response) {
+        return response;
+      });
+    })
+  );
+});
